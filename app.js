e564eadd885cf5af4d496358b5bd6aa33e8265b1
kelvinkirima014 <kelvinkirima2@gmail.com>
2021-01-11 18:23:42 +0300

    still working on it

---
+++ app.js
@@ -0,0 +1,8 @@
+//creates a css class to change hamburger menu
+//on click
+document.querySelector('.hamburger-menu')
+.addEventListener('click',()=>{
+    document.querySelector('.container').classList.toggle
+    ('change');
+
+})
